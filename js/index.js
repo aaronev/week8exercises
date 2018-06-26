2eed26ecd6552b66e039a252534961295166e957
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-25 20:03:59 -0700

    completes number 9

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@ document.getElementsByClassName("two")[0].style.backgroundColor="pink"
 var newDiv = document.createElement("div")
 
 newDiv.id="hello"
+
+var newH1 = document.createElement("h1")
+
+newH1.textContent="created new h1 element with text"
