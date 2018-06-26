e5cbcd85dc72d9e2a132567bb066060e879b2462
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-06-25 20:09:12 -0700

    finishes 11

--- js/index.js
+++ js/index.js
@@ -5,6 +5,11 @@ newDiv.id="hello"
 
 var newH1 = document.createElement("h1")
 
-newH1.textContent="created new h1 element with text"
+var h1WithTextContent = newH1.textContent="created new h1 element with text"
 
 document.getElementById('hello').appendChild(newH1)
+
+var newPTag = document.createElement("p")
+
+var pElementWithText = newPTag.textContent = "p text node"
+var h1WithTextNode = newH1.appendChild(pElementWithText)
